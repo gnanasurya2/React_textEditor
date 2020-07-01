934b2d602b55df51c73200e1d8d00241e8916339
gnanasurya <gnanasurya2@gmail.com>
2020-07-01 20:54:56 +0530

    Updates

---
+++ precache-manifest.9f9792882fb664a172bd39a417591fa8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4186806c5faf9bd53bf5a0ab9d3e571c",
+    "url": "/React_textEditor/index.html"
+  },
+  {
+    "revision": "8e8e3ba3ee96c3a0133d",
+    "url": "/React_textEditor/static/css/main.c40c6106.chunk.css"
+  },
+  {
+    "revision": "f08156a2135616214be9",
+    "url": "/React_textEditor/static/js/2.aa882571.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_textEditor/static/js/2.aa882571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8e3ba3ee96c3a0133d",
+    "url": "/React_textEditor/static/js/main.8916396c.chunk.js"
+  },
+  {
+    "revision": "b3e511f13aeafe070272",
+    "url": "/React_textEditor/static/js/runtime-main.adb74127.js"
+  },
+  {
+    "revision": "359120e3a71f430f822a7b40f9767894",
+    "url": "/React_textEditor/static/media/Apercu Bold.359120e3.otf"
+  },
+  {
+    "revision": "64a8711f9395e60ada4006943e11eed9",
+    "url": "/React_textEditor/static/media/ApercuLight.64a8711f.otf"
+  }
+]);
